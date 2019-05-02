4a89228b64ec97a4be0a583d55637ac0e4a2ec25
ericchase <eric.nalley+github@gmail.com>
2019-05-02 04:45:46 -0500

    pushing build system

---
+++ build/gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require( 'gulp' )
+const fs = require( 'fs' )
+const zip = require( 'gulp-zip' )
+
+gulp.task( 'default', () => {
+  let version = JSON.parse( fs.readFileSync( '../code/manifest.json' ) ).version
+  
+  return gulp.src( '../code/*' )
+    .pipe( zip( `reload-all-tabs-in-window-v${version}.zip` ) )
+    .pipe( gulp.dest( '../releases/' ) )
+} )
