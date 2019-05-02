eeb271545b47e9c8ae6c65650d016fea9b30b4bf
ericchase <eric.nalley+github@gmail.com>
2019-05-02 07:22:43 -0500

    updated gulpfile, added id to manifest, push release v0.1.1

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -5,7 +5,7 @@ const zip = require( 'gulp-zip' )
 gulp.task( 'default', () => {
   let version = JSON.parse( fs.readFileSync( '../code/manifest.json' ) ).version
   
-  return gulp.src( '../code/*' )
+  return gulp.src( '../code/**/*' )
     .pipe( zip( `reload-all-tabs-in-window-v${version}.zip` ) )
     .pipe( gulp.dest( '../releases/' ) )
 } )
